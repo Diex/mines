8a8bce1c48449c17e3075d3ff922e4ff0f5fe74d
Diego Alberti <developer.viset@gmail.com>
2023-11-30 10:28:19 -0300

    refactor(minesloop.py): change screen initialization to allow toggling between fullscreen and windowed mode fix(minesloop.py): fix indentation and add missing newline at end of file

--- minesloop.py
+++ minesloop.py
@@ -6,8 +6,8 @@ import glob
 
 # pygame setup
 pygame.init()
-pygame.display.set_mode((0,0), pygame.FULLSCREEN)
-screen = pygame.display.get_surface()
+fullscreen = False  # Flag to indicate if the screen is in fullscreen mode
+screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN if fullscreen else 0)
 clock = pygame.time.Clock()
 running = True
 
@@ -20,7 +20,10 @@ print(mov_files)
 cap = cv2.VideoCapture(mov_files[0])  # Use the first file in the list
 playing = False  # Flag to indicate if video is playing
 
+
 while running:
+
+
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
@@ -29,31 +32,39 @@ while running:
             cv2.destroyAllWindows()
             # running = False
         if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:                                
-                    cap.set(cv2.CAP_PROP_POS_FRAMES, 0)  # Go back to frame 0
-                    playing = True  # Start playing the video
-
+            if event.key == pygame.K_SPACE:                                
+                cap.set(cv2.CAP_PROP_POS_FRAMES, 0)  # Go back to frame 0
+                playing = True  # Start playing the video
+            if event.key == pygame.K_ESCAPE:                                
+                pygame.quit()
+                playing = False  # Start playing the video
+            if event.key == pygame.K_f:
+                fullscreen = not fullscreen  # Toggle fullscreen mode
+                screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN if fullscreen else 0)
 
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("black")
     
     # RENDER YOUR GAME HERE
     if playing:
-                ret, frame = cap.read()
-                if ret:
-                    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                    frame = np.rot90(frame)
+        ret, frame = cap.read()
+        if ret:
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            frame = np.rot90(frame)
 
-                    frame = pygame.surfarray.make_surface(frame)
+            frame = pygame.surfarray.make_surface(frame)
 
-                    window_center = (screen.get_width() // 2, screen.get_height() // 2)
-                    frame_pos = (window_center[0] - frame.get_width() // 2, window_center[1] - frame.get_height() // 2)
+            window_center = (screen.get_width() // 2, screen.get_height() // 2)
+            frame_pos = (window_center[0] - frame.get_width() // 2, window_center[1] - frame.get_height() // 2)
 
-                    screen.blit(frame, frame_pos)
-                    pygame.display.update()
+            screen.blit(frame, frame_pos)
+            pygame.display.update()
+    
     # flip() the display to put your work on screen
     pygame.display.flip()
-
+    # Clean up GPIO
+    
     clock.tick(60)  # limits FPS to 60
 
+# GPIO.cleanup()
 pygame.quit()
